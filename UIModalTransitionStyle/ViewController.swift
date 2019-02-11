bcf72452531aba1ed80555ccda5a663f4199e5cf
markfour <axhfaro@gmail.com>
2019-02-11 19:49:25 +0900

    Initial Commit

---
+++ UIModalTransitionStyle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIModalTransitionStyle
+//
+//  Created by kazumi hayashida on 2019/02/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
